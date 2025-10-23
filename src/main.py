351ad5967c26c4249135039d7f768c21d954b821
JuliCai <julian.halloran@gmail.com>
2025-10-23 15:29:11 -0400

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       jhalloran                                                    #
+# 	Created:      10/23/2025, 3:26:18 PM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
